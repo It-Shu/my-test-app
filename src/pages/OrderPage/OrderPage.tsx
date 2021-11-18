e7158b6cc4c8ae8dc22db98e4f04df6b5087ad5c
It-Shu <it.shustov@gmail.com>
2021-11-18 22:12:42 +0300

    css refactor

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {useParams} from "react-router-dom";
-import s from './OrderPage.module.css'
+import s from '../../components/Table/TableData/TableData.module.sass'
 
 
 export const OrderPage: FC = () => {
@@ -9,21 +9,43 @@ export const OrderPage: FC = () => {
 
 
     return (
-        <div>
-           <h1 className={s.title}>ORDER PAGE</h1>
-            <div className={s.orderPage}>
-                <div className={s.orderItem}>Номер</div>
-                <div className={s.orderItem}>Тип задания</div>
-                <div className={s.orderItem}>Аккаунт</div>
-                <div className={s.orderItem}>Статус</div>
-            </div>
-            <div className={s.orderPage}>
-                <div className={s.orderItem}>{id}</div>
-                <div className={s.orderItem}>{order_type}</div>
-                <div className={s.orderItem}>{account}</div>
-                <div className={s.orderItem}>{status}</div>
-            </div>
+        <>
+            <h1 className={s.title}>ORDER PAGE</h1>
+            <table className={s.contentTable}>
+                <thead>
+                <tr className={s.headTR}>
+                    <th>Номер</th>
+                    <th>Тип задания</th>
+                    <th>Аккаунт</th>
+                    <th>Статус</th>
+                </tr>
+                </thead>
+                <tbody className={s.tableBody}>
+                <tr>
+                    <td>{id}</td>
+                    <td>{order_type}</td>
+                    <td>{account}</td>
+                    <td>{status}</td>
+                </tr>
+                </tbody>
+            </table>
+        </>
 
-        </div>
-    );
+    // <div>
+    //     <div className={s.orderPage}>
+    //         <div className={s.orderItem}>Номер</div>
+    //         <div className={s.orderItem}>Тип задания</div>
+    //         <div className={s.orderItem}>Аккаунт</div>
+    //         <div className={s.orderItem}>Статус</div>
+    //     </div>
+    //     <div className={s.orderPage}>
+    //         <div className={s.orderItem}>{id}</div>
+    //         <div className={s.orderItem}>{order_type}</div>
+    //         <div className={s.orderItem}>{account}</div>
+    //         <div className={s.orderItem}>{status}</div>
+    //     </div>
+    //
+    // </div>
+)
+    ;
 };
