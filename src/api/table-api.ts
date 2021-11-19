0c933b90b62aa3e53fc93f4f831fbcc46157807c
It-Shu <it.shustov@gmail.com>
2021-11-20 00:24:35 +0300

    api

--- src/api/table-api.ts
+++ src/api/table-api.ts
@@ -28,6 +28,6 @@ export type TableItemsResponseType = {
 
 export const orderAPI = {
     getOrder: () => axios
-        .get<TableItemsResponseType[]>('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9f15021c-fcd4-4657-aff4-2782f62b60b6/test_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211118%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211118T131127Z&X-Amz-Expires=86400&X-Amz-Signature=ad28d2bafcc9d9d7cfcd6adabb47221af371270397504911f5f45c23882cc04a&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22test_data.json%22&x-id=GetObject')
+        .get<TableItemsResponseType[]>('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9f15021c-fcd4-4657-aff4-2782f62b60b6/test_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211119%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211119T212354Z&X-Amz-Expires=86400&X-Amz-Signature=bf3a7e68313475dc8964fe502ce54f2fdcc35a7e0e8dee9df549c6c5e248cd17&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22test_data.json%22&x-id=GetObject')
 
 }
