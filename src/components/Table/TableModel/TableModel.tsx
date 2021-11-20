090f0125f3f42c507c2a2aa85283717d17cca523
It-Shu <it.shustov@gmail.com>
2021-11-20 23:44:41 +0300

    added css style

--- src/components/Table/TableModel/TableModel.tsx
+++ src/components/Table/TableModel/TableModel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {TableModel} from "../TableData/TableData";
 import {TableItemsResponseType} from "../../../api/table-api";
-import ss from './TableModel.module.sass'
+import s from './TableModel.module.sass'
 import {PATH} from '../../../routes/routes';
 import {NavLink} from 'react-router-dom';
 import Statuses from "../Statuses/Statuses";
@@ -11,13 +11,13 @@ import Statuses from "../Statuses/Statuses";
 export const tableModel = (): TableModel[] => [
     {
         header: index =>
-            <th key={'id-title-' + index} className={ss.headerTH}>
+            <th key={'id-title-' + index} className={s.headerTH}>
                 Номер / Дата
             </th>,
 
         body: (item: TableItemsResponseType) =>
 
-            <td key={'id-cell-' + item.id} className={ss.bodyTD}>
+            <td key={'id-cell-' + item.id} className={s.bodyTD}>
                 <NavLink
                     to={PATH.ORDER_PAGE + '/' + item.id + '/' + item.order_type.name + '/' + item.account.name + '/' + item.status}>
                     {item.id}
@@ -30,11 +30,11 @@ export const tableModel = (): TableModel[] => [
     },
     {
         header: index =>
-            <th key={'order-title-' + index} className={ss.headerTH}>
+            <th key={'order-title-' + index} className={s.headerTH}>
                 Тип задания / Автор
             </th>,
         body: (item: TableItemsResponseType) =>
-            <td key={'order-cell-' + item.order_type.name} className={ss.bodyOrderTD}>
+            <td key={'order-cell-' + item.order_type.name} className={s.bodyOrderTD}>
                 {item.order_type.name}
                 <br/>
                 {item.created_user.name}
@@ -42,24 +42,24 @@ export const tableModel = (): TableModel[] => [
     },
     {
         header: index =>
-            <th key={'account-title-' + index} className={ss.headerTH}>
+            <th key={'account-title-' + index} className={s.headerTH}>
                 Аккаунт / Терминал
             </th>,
         body: (item: TableItemsResponseType) =>
 
-            <tr key={'account-cell-' + item.account.name} className={ss.bodyTD}>
-                <td className={ss.accountTerminal}>{item.account.name}</td>
+            <tr key={'account-cell-' + item.account.name} className={s.bodyTD}>
+                <td className={s.accountTerminal}>{item.account.name}</td>
 
-                <td className={ss.accountTerminal}>{item.terminal.name}</td>
+                <td className={s.accountTerminal}>{item.terminal.name}</td>
             </tr>
     },
     {
         header: index =>
-            <th key={'status-title-' + index} className={ss.headerTH}>
+            <th key={'status-title-' + index} className={s.headerTH}>
                 Статус
             </th>,
         body: (item: TableItemsResponseType) =>
-            <td key={'status-cell-' + item.status} className={ss.bodyTD}>{
+            <td key={'status-cell-' + item.status} className={s.bodyTD}>{
                 <Statuses data={item}/>
             }</td>
     },
